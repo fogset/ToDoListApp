e4e93fe24e1a64684dd8dfe776c747bac0ba55b7
Administrator <zhangth1990@hotmail.com>
2018-05-31 22:12:58 -0600

    added datasource to the new tableView

--- Todoey/Controllers/CategoryTableViewController.swift
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -12,18 +12,56 @@ import CoreData
 
 class CategoryTableViewController: UITableViewController {
 
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadItems()
         
     }
     //MARK: -TableView Datasource Methods
-    
+ 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+    }
     //MARK: -TableView Delegate Methods
     
     //MARK: -Add New categories
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
         
+        let alert = UIAlertController(title: " Add New Todoey Item", message: "",preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) {(action) in
+            // what will happen once the user clicks the Add Item button on our UIAlert
+            
+            
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            self.itemArray.append(newItem)
+            self.saveItems()
+            
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            print("alertText get triggered")
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     //MARK: -TableView Datasource Methods
@@ -31,6 +69,28 @@ class CategoryTableViewController: UITableViewController {
     //MARK: -TableView Delegate Methods
     
     //MARK: -TableView Manipulation Methods
+    //MARK: -Model Manupulation Methods
+    func saveItems(){
+        //save added item into user defaults
+        
+        do{
+            
+            try context.save()
+        }catch{
+            print("Error saving context\(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+        
+        do {
+            itemArray = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
     
     
 }
