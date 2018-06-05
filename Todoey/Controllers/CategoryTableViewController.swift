c7fe0c72bd3d355edf5bb86f1bc81e618519e7bc
Administrator <zhangth1990@hotmail.com>
2018-06-04 23:32:10 -0600

    passed selected categories to different itemViewController

--- Todoey/Controllers/CategoryTableViewController.swift
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -35,6 +35,17 @@ class CategoryTableViewController: UITableViewController {
         return cell
     }
     //MARK: -TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        if let indexPath = tableView.indexPathForSelectedRow{
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+        
+    }
     
     //MARK: -Model Manupulation Methods
     func saveCategories(){
--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,13 +11,17 @@ import CoreData
 
 class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
+    var selectedCategory : Category? {
+        didSet{
+            loadItems()
+        }
+    }
     
      let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadItems()
 
     }
     
@@ -70,6 +74,7 @@ class TodoListViewController: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             self.saveItems()
 
